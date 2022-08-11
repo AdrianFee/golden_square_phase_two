e058724f5951f8b430ed1af1da84e1ddab4478ff
Adrian Fee <adrian.fee@yapily.com>
2022-08-11 09:55:04 +0100

    with hash mapped to string

--- lib/design_single_class_program_challenge.rb
+++ lib/design_single_class_program_challenge.rb
@@ -4,28 +4,17 @@ class MyMusic
         @name = name
         @music_list = {}
     end 
-
     def add_song(song, artist)
         fail "Don't forget to add the artist!" if ((song.nil?) || (artist.nil?))
         @music_list[artist] = song
         return "Hope you've been enjoying #{artist}!"
     end 
-
     def all_my_music
         if @music_list.count == 0
             return "You haven't listened to anything yet."
         elsif @music_list.count != 0
             sentence = ["Here's what you've listened to so far:"]
-            i = 0
-            k = 0
-            (@music_list.count).times do
-                sentence << " - #{@music_list.to_a[i][k]}: "
-                i += 0
-                k += 1
-                sentence << "#{@music_list.to_a[i][k]}\n"
-                i += 1
-                k -= 1
-            end 
+            sentence << (@music_list.map{|k,v| " - #{k}: #{v}\n"})
             return sentence.join
         else 
         end 
