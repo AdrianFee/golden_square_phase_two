c35a131d08c8f8844275cf3cb414849a573aec43
Adrian Fee <adrian.fee@yapily.com>
2022-08-11 09:46:06 +0100

    before changing hash to string thing

--- lib/design_single_class_program_challenge.rb
+++ lib/design_single_class_program_challenge.rb
@@ -6,10 +6,28 @@ class MyMusic
     end 
 
     def add_song(song, artist)
-        # song = nil 
-        # artist = nil
-        # fail "Don't forget to add the artist!" if ((song.nil? == true) || (artist.nil? == true))
+        fail "Don't forget to add the artist!" if ((song.nil?) || (artist.nil?))
         @music_list[artist] = song
         return "Hope you've been enjoying #{artist}!"
+    end 
+
+    def all_my_music
+        if @music_list.count == 0
+            return "You haven't listened to anything yet."
+        elsif @music_list.count != 0
+            sentence = ["Here's what you've listened to so far:"]
+            i = 0
+            k = 0
+            (@music_list.count).times do
+                sentence << " - #{@music_list.to_a[i][k]}: "
+                i += 0
+                k += 1
+                sentence << "#{@music_list.to_a[i][k]}\n"
+                i += 1
+                k -= 1
+            end 
+            return sentence.join
+        else 
+        end 
     end 
 end 
--- spec/design_single_class_program_challenge_spec.rb
+++ spec/design_single_class_program_challenge_spec.rb
@@ -1,17 +1,30 @@
 require "design_single_class_program_challenge"
 
 RSpec.describe do 
-    # context "when no artist has been added" do 
-    #     it "fails" do
-    #         new_songs = MyMusic.new("Adrian")
-    #         expect {new_songs.add_song("Loco in Acapulco", " ")}.to raise_error "Don't forget to add the artist!"
-    #     end 
-    # end 
+    context "when no artist has been added" do 
+        it "fails" do
+            new_songs = MyMusic.new("Adrian")
+            expect {new_songs.add_song("Loco in Acapulco", nil)}.to raise_error "Don't forget to add the artist!"
+        end 
+        it "fails" do
+            new_songs = MyMusic.new("Adrian")
+            expect {new_songs.add_song(nil, "Four Tops")}.to raise_error "Don't forget to add the artist!"
+        end 
+    end 
     context "when an artist and a song has been added" do 
         it "adds them to the list" do 
             new_songs = MyMusic.new("Adrian")
             result = new_songs.add_song("Loco in Acapulco", "Four Tops")
             expect(result).to eq "Hope you've been enjoying Four Tops!"
+        end 
+    end 
+    context "when several artists and songs have been added, returns a formatted list" do 
+        it "returns a list of artists and songs" do 
+            new_songs = MyMusic.new("Adrian")
+            new_songs.add_song("Loco in Acapulco", "Four Tops")
+            new_songs.add_song("Round Round", "Sugababes")
+            new_songs.add_song("Bohemian Rhapsody", "Queen")
+            expect(new_songs.all_my_music).to eq "Here's what you've listened to so far: - Four Tops: Loco in Acapulco\n - Sugababes: Round Round\n - Queen: Bohemian Rhapsody\n"
         end 
     end 
 end 
